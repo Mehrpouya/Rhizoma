f7844de17a37019400bc2d04d555b1ab24e67819
mehrpouya <h517352@uad.ac.uk>
2024-08-28 12:21:29 +0100

    adding my changes

--- Assets/Scripts/RhizomaVoices.cs
+++ Assets/Scripts/RhizomaVoices.cs
@@ -10,6 +10,7 @@ using System.Collections;
 
 public class RhizomaVoices : MonoBehaviour
     {
+    public string S = "Hi";
     public TTSPlayer _player;
     public AudioSource source;
 
@@ -37,10 +38,10 @@ public class RhizomaVoices : MonoBehaviour
 
     }
     async void SayIt() {
-        await _player.Speak("let's talk about joy later. They got such mixed-up life that it would take a Iong time to get to know them properly It felt ages, i did really missed you. i also managed to have a good time by myself. Time was bending in my metal intestines, still enjoying our little laughter together,. Are you ok? You seem Little anxious or tense, i can just feel it. I'm sorry i didn't mean to introu hahahah ahahahahahahahahahahahaha hah\r\n\r\nhah hahahaha hahhhhhhhhhh\r\n\r\nother irony. intrude also got crush at the end of last line, not including my yful, running and skating and parkouring over the last line. Oh excuse me, cum-- I beg your parden, Excuse me, here, yes down here. Do you mind using that hard, cold, metal teeth over my tuner, please hurry up, but also take your time. Enjoy it you know. But please let’s remember about boundaries. Hello CellloW, He he hehehe Hello....HHHHHoooohh ecelelelelhohoooo, Hello# HELLO! rry, I forgot to introduce myself. Remember, they don't like to get picked up. ps, de just missed to givell him another energy boost. You know what! re's an idea. they ran out of energy, so why not use the time to talk more abo out us? and then give them a ring or energy boost or whatever you wanna call t, they'd be back and it'd be like nothing happened. But that means we get toh other a little bit more. Well do I ever reminicse about some cool, or provoking, bending, hammering, touching, infuriating sentences. well\r\n");
+        await _player.Speak(S);
         AudioClip audioClip = _player.source.clip;
         source.clip = audioClip;
-        source.loop = false;
+        source.loop = true;
         source.Play();
     }
 }
